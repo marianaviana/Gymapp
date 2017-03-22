aafd166cd860474e05cffb1a88dd4414ef5b8b3a
Luiza Melo <meloluiza@live.com>
2017-03-22 20:36:14 -0300

    Traduções

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,21 @@
 class Employee < ApplicationRecord
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+	validates :name, presence: true
+	validates :position, presence: true
+	validates :birth_date, presence: true
+	validates :rg, presence: true
+	validates :cpf, presence: true
+	validates :cpf, uniqueness: true
+	validates :telephone, presence: true
+	validates :admission_date, presence: true
+
+	before_save :upcase_name
+
+	def upcase_name
+		self.name = self.name.upcase
+	end
+
+
 end
--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,10 +1,14 @@
 class Equipment < ApplicationRecord
-  validates :name, presence: true
-  validates :name, uniqueness: true
+	validates :name, presence: true
+	validates :name, uniqueness: true
+	validates :quantity, presence: true
+	validates :aquisition_date, presence: true
+	validates :weight, presence: true
+	validates :max_weight, presence: true
 
-  before_save :upcase_name
+	before_save :upcase_name
 
-  def upcase_name
-    self.name = self.name.upcase
-  end
+	def upcase_name
+		self.name = self.name.upcase
+	end
 end
